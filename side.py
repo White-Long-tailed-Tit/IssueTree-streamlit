ce85153b31b23708eb6aa6e66fd82faa8eb290ae
coldnoodle_kjy <kjy11069@sookmyung.ac.kr>
2024-02-23 00:54:39 +0900

    feat: sos 기능 추가

--- side.py
+++ side.py
@@ -4,22 +4,17 @@ import datetime
 from streamlit_option_menu import option_menu
 from streamlit_tags import st_tags
 import streamlit.components.v1 as components
+import es_con 
+import sos
+import json
+import re 
 
 def main_page():
     #title
     st.markdown("<h1 style='text-align: center; color: black;'>IssueTree Search</h1>", unsafe_allow_html=True)
 
-
-    data = {
-        'document': [
-            "Exception in thread 'main' java.lang.UnsupportedClassVersionError.txt",
-            "could not prepare statement; SQL; nested exception is org.hibernate.exception.SQLGrammarException.txt",
-            "variable not initialized in the default constructor 에러, class lombok.javac.apt.lombokprocessor cannot access class .txt"
-        ],
-        'springboot version': [3.1, 2.2, 2.4],
-        'library': ['JPA', 'lombok', 'jdbc'],
-        'date': ['2024.02.02', '2022.01.11', '2021.01.01'],
-    }
+    data=es_con.connect() #elasticsearch 연결 후 전체 데이터 목록 
+    
     df = pd.DataFrame(data)
 
     #text input: 오류 메시지 input
@@ -28,8 +23,10 @@ def main_page():
         placeholder='오류메시지를 입력해주세요 ',
         help='Help message goes here'
     )
+    # 입력된 문자열을 raw string으로 변환
+    search_text = r'''{}'''.format(search_text)
 
-    options = df['document'].unique().tolist()
+    #options = df['document'].unique().tolist()
 
     #filter
     with st.expander('Filter'):
@@ -63,18 +60,18 @@ def main_page():
         
 
     # 입력한 텍스트를 포함하는 문서 필터링
-    filtered_df = df[df['document'].str.contains(search_text)]
+    filtered_df = df[df['issue'].str.contains(search_text)]
 
     # 추가한 태그에 따라 필터링
     if len(tag_library) > 0:
-        filtered_df = filtered_df[filtered_df['library'].apply(lambda x: any(tag.lower() in x.lower() for tag in tag_library))]
+        filtered_df = filtered_df[filtered_df['stack'].apply(lambda x: any(tag.lower() in x.lower() for tag in tag_library))]
 
     if len(tag_springbootversion) > 0:
-        filtered_df = filtered_df[filtered_df['springboot version'].astype(str).apply(lambda x: any(tag.lower() in x.lower() for tag in tag_springbootversion))]
+        filtered_df = filtered_df[filtered_df['version'].astype(str).apply(lambda x: any(tag.lower() in x.lower() for tag in tag_springbootversion))]
 
     a_date_start = datetime.datetime.combine(a_date[0], datetime.datetime.min.time())
     a_date_end = datetime.datetime.combine(a_date[1], datetime.datetime.max.time())
-    filtered_df = filtered_df[pd.to_datetime(filtered_df['date']).between(a_date_start, a_date_end)]
+    #filtered_df = filtered_df[pd.to_datetime(filtered_df['lastModifiedDate']).between(a_date_start, a_date_end)]
 
     #부제목(결과)
     st.subheader('results')
@@ -97,23 +94,66 @@ def main_page():
         st.write("We couldn't find anything :sob:")
         # sos 버튼
         st.subheader('need help?')
+
+        # 에러 메시지 parsing
+        package, file_path, isLine,line =sos.parsing(search_text)
+        author_name=None
+        author_email=None
+        
+        
+
+        if not(package==None or file_path==None): #패키지명 파싱 가능한 경우
+            # GitHub 저장소 정보
+            owner = 'White-Long-tailed-Tit'
+            repo = 'IssueTree-Spring'
+            branch = 'main'
+            token = ''
+
+            if isLine: #라인 넘버 추출 가능한 오류 메시지인 경우 
+                blame_data=sos.get_git_blame_line(owner, repo, file_path, token, branch)
+                # JSON 문자열을 파이썬 딕셔너리로 변환
+                blame_data_dict = json.loads(blame_data)
+
+                # line 값이 있는지 확인하고 해당하는 author의 name과 email 추출
+                for range_data in blame_data_dict['data']['repository']['object']['blame']['ranges']:
+                    if line >= range_data['startingLine'] and line <= range_data['endingLine']:
+                        author_name = range_data['commit']['author']['name']
+                        author_email = range_data['commit']['author']['email']
+                        print("Author Name:", author_name)
+                        print("Author Email:", author_email)
+                        break
+            else: #라인 넘버 추출 x, 패키지명만 가능한 경우 
+                final_commit_data=sos.get_bit_blame(owner, repo, file_path, token, branch)
+                final_commit_dict=json.loads(final_commit_data)
+
+                author_name = final_commit_dict['author']
+                author_email = final_commit_dict['email']
+
+                print("Author:", author_name)
+                print("Email:", author_email)
+            
+
         with st.expander('**Request Body**'):
             with st.form('Request Body'):
-                st.text_input('Reporter Name')
-                st.text_input('Package Name')
-                st.text_input('Error Message')
-                st.text_input('Manager Github Id')
-                st.text_input('Comment')
-                st.text_input('Stack')
-                st.text_input('Version')
-
+                reporter_name=st.text_input('Reporter Name')
+                package_input=st.text_input('Package Name',value=package)
+                error_msg=st.text_input('Error Message',value=search_text)
+                manager=st.text_input('Manager Github Id',value=author_name)
+                comment=st.text_input('Comment')
+                stack=st.text_input('Stack')
+                version=st.text_input('Version')
+              
                 #submit button
                 submitted = st.form_submit_button('SOS')
+                if submitted:
+                    sos.send_form(reporter_name,package_input,error_msg,manager,comment,
+                              stack,version)
+
 
     else:
         selection = dataframe_with_selections(df)
         if selection['selected_rows'] is not None and not selection['selected_rows'].empty:
-            doc_name = selection['selected_rows'].iloc[0]['document']
+            doc_name = selection['selected_rows'].iloc[0]['issue']
             st.write(f"Selected Document: {doc_name}")
 
 
--- sos.py
+++ sos.py
@@ -14,4 +14,219 @@ SOS 버튼 (요청) 누르면 -> 해당 에러 메시지를 파싱
 '''
 
 import streamlit as st
+import requests
+import re
 
+# GitHub 저장소 정보
+'''
+owner = 'White-Long-tailed-Tit'
+repo = 'IssueTree-demo'
+branch = 'main'
+base_file_path='com/example/demo/'
+'''
+
+#test용 
+owner = 'White-Long-tailed-Tit'
+repo = 'IssueTree-Spring'
+branch = 'main'
+base_file_path='com\wltt\issuetree'
+
+packages=['question','Member']
+sub_packages=['controller','domain','repository','service']
+
+
+def parsing(search_text):
+    isrepo=False
+    isfile=False
+    isLine=False #라인 넘버 추출가능한 에러인가? 
+    line=None
+
+    # 레포지토리명 존재 판단 
+    if repo in search_text:
+        isrepo=True
+    
+    
+    # 파일 위치 구조명 존재 판단 
+    if base_file_path in search_text:
+        isfile=True
+
+    # # controller or domain 등등 하위 폴더 구조명이 발견되는지 판단 
+    # for sub in sub_packages:
+    #     if sub in search_text:
+    #         issub=True
+    #         sub_path=sub
+
+    # 패키지명과 검색 문자열이 모두 존재하는지 확인하고 패키지명 반환
+    if isrepo and isfile: #모두 True이면 
+        for package in packages:
+            if package in search_text:
+                pre_file_path=base_file_path+"\\"+package+"\\" #com\wltt\issuetree\question\controller
+                print(pre_file_path)
+                
+
+                if pre_file_path in search_text:
+                    index = search_text.find(pre_file_path)
+                    if index != -1:
+                        target_index = index + len(pre_file_path)
+                        file_name_index = search_text.find(".", target_index)
+                        if file_name_index != -1:
+                            end_index = file_name_index
+                            if ':' in search_text[file_name_index:]:
+                                colon_index = search_text.find(':', file_name_index)
+                                if colon_index + 1 < len(search_text):
+                                    # 숫자가 나오는 부분까지만 추출
+                                    end_num_index = colon_index + 1
+                                    while end_num_index < len(search_text) and search_text[end_num_index].isdigit():
+                                        end_num_index += 1
+                                    if colon_index + 1 != end_num_index:  # 숫자가 없는 경우 처리
+                                        line = int(search_text[colon_index + 1:end_num_index])
+                                    else:
+                                        line = None
+                                    end_index = colon_index
+                                    isLine = True
+                                else:
+                                    isLine = False
+                                    line = None
+                            else:
+                                isLine = False
+                                line = None
+                            print(search_text[target_index:end_index])
+                            print("isLine:", isLine)
+
+
+                            file_name=search_text[target_index:file_name_index]
+                            print(search_text[target_index:file_name_index]) #controller\MemberController 파싱 
+                
+                # for example file_structure: src/main/java/com/wltt/issuetree/question/controller
+                file_path="src\\main\\java\\"+pre_file_path+file_name+".java"
+                package="com.wltt.issuetree."+package
+                file_path=file_path.replace("\\", "/")
+                print(file_path)
+                return package, file_path, isLine,line 
+    
+    return None, None, isLine, None
+
+
+# parsing 테스트 
+error_message_example = """
+org.springframework.beans.factory.UnsatisfiedDependencyException: Error creating bean with name 'questionController' defined in file [C:\\github\\IssueTree-Spring\\out\\production\\classes\\com\\wltt\\issuetree\\question\\controller\\QuestionController.class]: Unsatisfied dependency expressed through constructor parameter 0; nested exception is org.springframework.beans.factory.UnsatisfiedDependencyException: Error creating bean with name 'questionService' defined in file [C:\\github\\IssueTree-Spring\\out\\production\\classes\\com\\wltt\\issuetree\\question\\service\\QuestionService.class]: Unsatisfied dependency expressed through constructor parameter 0; nested exception is org.springframework.beans.factory.BeanCreationException: Error creating bean with name 'questionRepository' defined in com.wltt.issuetree.question.domain.repository.QuestionRepository defined in @EnableElasticsearchRepositories declared on ElasticsearchRepositoriesRegistrar.EnableElasticsearchRepositoriesConfiguration: Invocation of init method failed; nested exception is org.springframework.beans.BeanInstantiationException: Failed to instantiate [org.springframework.data.elasticsearch.repository.support.SimpleElasticsearchRepository]: Constructor threw exception; nested exception is org.springframework.data.elasticsearch.UncategorizedElasticsearchException: java.util.concurrent.ExecutionException: java.net.ConnectException: Timeout connecting to [682fb71161354b0d812e6c2d5f5fc8ca.us-west-2.aws.found.io/52.26.59.44:443]; nested exception is ElasticsearchException[java.util.concurrent.ExecutionException: java.net.ConnectException: Timeout connecting to [682fb71161354b0d812e6c2d5f5fc8ca.us-west-2.aws.found.io/52.26.59.44:443]]; nested: ExecutionException[java.net.ConnectException: Timeout connecting to [682fb71161354b0d812e6c2d5f5fc8ca.us-west-2.aws.found.io/52.26.59.44:443]]; nested: ConnectException[Timeout connecting to [682fb71161354b0d812e6c2d5f5fc8ca.us-west-2.aws.found.io/52.26.59.44:443]];
+"""
+package,file_path,isLine,line=parsing(error_message_example)
+print(package)
+print(file_path)
+print(line)
+
+#parsing test : line 넘버 추출 가능한 경우 
+# error_message_example2='''
+# C:\Users\user\Desktop\휴학\IssueTree-demo\demo\src\main\java\com\example\demo\controller\MemberController.java:4: error: package com.example.demo.service does not exist
+# import com.example.demo.service.MemberService;'''
+
+
+
+
+
+def get_git_blame_line(owner, repo, file_path, token, branch):
+    headers = {
+        'Authorization': f'token {token}'
+    }
+    
+    # GraphQL 쿼리
+    query = """
+    query GetGitBlame($owner: String!, $repo: String!, $file_path: String!, $branch: String!) {
+      repository(owner: $owner, name: $repo) {
+        object(expression: $branch) {
+          ... on Commit {
+            blame(path: $file_path) {
+              ranges {
+                startingLine
+                endingLine
+                age
+                commit {
+                  oid
+                  author {
+                    name
+                    email
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    """
+    
+    variables = {
+        "owner": owner,
+        "repo": repo,
+        "file_path": file_path,
+        "branch": branch
+    }
+    
+    response = requests.post('https://api.github.com/graphql', json={'query': query, 'variables': variables}, headers=headers)
+    
+    if response.status_code == 200:
+        blame_data = response.json()
+        return blame_data
+    else:
+        print(f"Failed to retrieve Git blame data: {response.status_code}")
+        return None
+
+def get_bit_blame(owner, repo, file_path, token, branch):
+    # API 요청 보내기
+    url = f'https://api.github.com/repos/{owner}/{repo}/commits?path={file_path}&sha={branch}'
+    headers = {'Authorization': f'token {token}'}
+    response = requests.get(url, headers=headers)
+
+    # 응답 처리
+    if response.status_code == 200:
+        commits = response.json()
+        for commit in commits:
+            commit_info = {
+                'commit_sha': commit['sha'],
+                'author': commit['commit']['author']['name'],
+                'email': commit['commit']['author']['email'],
+                'message': commit['commit']['message']
+            }
+            print(commit_info)
+            return commit_info
+    else:
+        print(f'API 요청에 실패했습니다. 상태 코드: {response.status_code}')
+
+'''
+# 파일 경로
+file_path = 'demo/src/main/java/com/example/demo/Member/controller/MemberController.java'
+
+# 사용자 인증 토큰
+token = '당신의_사용자인증토큰을_넣어주세요'
+
+# Git blame 정보 가져오기
+blame_data = get_git_blame(owner, repo, file_path, token, branch)
+if blame_data:
+    # Git blame 정보 출력
+    print(json.dumps(blame_data, indent=2))
+'''
+
+def send_form(reporter_name,error_message,comment,stack,version,package_name=None,manager_github_id=None):
+    # POST 요청 보낼 URL
+    url = 'http://ec2-43-202-41-32.ap-northeast-2.compute.amazonaws.com/api/v1/reports'  # 서버의 실제 URL로 변경해야 합니다.
+
+    # POST 요청 보낼 데이터
+    data = {
+        'reporter_name': reporter_name,
+        'package_name': package_name,
+        'error_message': error_message,
+        'manager_github_id': manager_github_id,
+        'comment': comment,
+        'stack': stack,
+        'version': version
+    }
+
+    # POST 요청 보내기
+    response = requests.post(url, data=data)
+
+    # 응답 확인
+    if response.status_code == 200:
+        print('POST 요청이 성공적으로 보내졌습니다.')
+    else:
+        print('POST 요청이 실패하였습니다. 상태 코드:', response.status_code)
